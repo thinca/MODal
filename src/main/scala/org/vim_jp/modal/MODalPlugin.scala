de56828e185053ed028826837c338af30ae46f3c
thinca <thinca@gmail.com>
2021-06-10 20:28:35 +0900

    Sort imports

--- src/main/scala/org/vim_jp/modal/MODalPlugin.scala
+++ src/main/scala/org/vim_jp/modal/MODalPlugin.scala
@@ -1,12 +1,12 @@
 package org.vim_jp.modal
 
 import org.bukkit.Material
+import org.bukkit.Server
 import org.bukkit.block.Block
+import org.bukkit.entity.Player
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.{Listener, EventHandler}
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.entity.Player
-import org.bukkit.Server
 import org.bukkit.scheduler.BukkitRunnable
 
 class MODalPlugin extends JavaPlugin:
