896aba74486d9efab88e8b9465aa40b3102aa9ab
kyoh86 <me@kyoh86.dev>
2023-04-24 22:28:26 +0900

    style: Format code by Scalafmt

--- src/main/scala/org/vim_jp/modal/MODalPlugin.scala
+++ src/main/scala/org/vim_jp/modal/MODalPlugin.scala
@@ -6,9 +6,10 @@ import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
 import org.bukkit.entity.SpectralArrow
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.entity.ProjectileHitEvent
-import org.bukkit.event.{Listener, EventHandler}
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 
@@ -23,10 +24,12 @@ class MODalPlugin extends JavaPlugin:
 
   class Kikori extends Listener:
     def isLog(block: Block): Boolean =
-      block.getType.name.endsWith("_LOG") || block.getType.name.endsWith("_STEM")
+      block.getType.name.endsWith("_LOG") || block.getType.name.endsWith(
+        "_STEM"
+      )
 
     def tryBreak(player: Player, block: Block): Unit =
-      new BukkitRunnable:
+      new BukkitRunnable {
         override def run(): Unit =
           if !player.isValid || !isLog(block) then return
 
@@ -34,12 +37,11 @@ class MODalPlugin extends JavaPlugin:
           val underBlocks = for
             x <- -1 to 1
             z <- -1 to 1
-          yield
-            block.getRelative(x, y, z)
+          yield block.getRelative(x, y, z)
           if underBlocks.forall(!_.getType.isOccluding) then
             block.breakNaturally()
             onLogBreak(player, block)
-      .runTaskLater(outer, 1)
+      }.runTaskLater(outer, 1)
 
     def onLogBreak(player: Player, block: Block): Unit =
       for
@@ -73,11 +75,11 @@ class MODalPlugin extends JavaPlugin:
 
       val arrow = projectile match
         case arrow: SpectralArrow => arrow
-        case _ => return
+        case _                    => return
 
       val player = arrow.getShooter match
         case p: Player => p
-        case _ => return
+        case _         => return
 
       val hitBlock = event.getHitBlock
       if hitBlock == null then return
@@ -85,6 +87,11 @@ class MODalPlugin extends JavaPlugin:
       val warpPos = hitBlock.getRelative(event.getHitBlockFace)
 
       if warpPos.getRelative(BlockFace.DOWN).getType.isSolid &&
-          isSafeBlock(warpPos) && isSafeBlock(warpPos.getRelative(BlockFace.UP)) then
+        isSafeBlock(warpPos) && isSafeBlock(warpPos.getRelative(BlockFace.UP))
+      then
         arrow.remove()
-        player.teleport(warpPos.getLocation.add(0.5, 0, 0.5).setDirection(player.getLocation.getDirection))
+        player.teleport(
+          warpPos.getLocation
+            .add(0.5, 0, 0.5)
+            .setDirection(player.getLocation.getDirection)
+        )
