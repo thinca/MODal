063cfe588a86848f36855ed77a62dc30dcdbc031
kyoh86 <me@kyoh86.dev>
2023-05-23 21:05:43 +0900

    impolements teleporter-mode

--- src/main/scala/org/vim_jp/modal/MODalPlugin.scala
+++ src/main/scala/org/vim_jp/modal/MODalPlugin.scala
@@ -1,22 +1,16 @@
 package org.vim_jp.modal
 
 import org.bukkit.ChatColor
-import org.bukkit.Material
 import org.bukkit.NamespacedKey
-import org.bukkit.block.Block
-import org.bukkit.block.BlockFace
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import org.bukkit.entity.SpectralArrow
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.entity.ProjectileHitEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.vim_jp.modal.mode.FarmerMode
 import org.vim_jp.modal.mode.KikoriMode
 import org.vim_jp.modal.mode.ModeChanging
+import org.vim_jp.modal.mode.TeleporterMode
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -31,14 +25,13 @@ class MODalPlugin extends JavaPlugin:
   val modeNameDataKey = NamespacedKey(outer, MODalPlugin.MODE_NAME_DATA_KEY)
   val modeCapacityDataKey =
     NamespacedKey(outer, MODalPlugin.MODE_CAPACITY_DATA_KEY)
-  val modes = Set(FarmerMode(this), KikoriMode(this))
+  val modes = Set(FarmerMode(this), KikoriMode(this), TeleporterMode(this))
 
   override def onEnable(): Unit =
     val server = getServer
 
     val pluginManager = server.getPluginManager
     pluginManager.registerEvents(ModeChanging(this), this)
-    pluginManager.registerEvents(ArrowWarp(), this)
     modes.foreach(m => {
       ModeChanging.registerMode(m)
       pluginManager.registerEvents(m, this)
@@ -83,35 +76,3 @@ class MODalPlugin extends JavaPlugin:
     def action(player: Player, args: Array[String]): Boolean =
       modes.filter(m => m.isActive(player)).foreach(m => m.inactivate(player))
       true
-
-  class ArrowWarp extends Listener:
-    def isSafeBlock(block: Block): Boolean =
-      val blockType = block.getType
-      block.isPassable && blockType != Material.WATER && blockType != Material.LAVA
-
-    @EventHandler
-    def onProjectileHit(event: ProjectileHitEvent): Unit =
-      val projectile = event.getEntity
-
-      val arrow = projectile match
-        case arrow: SpectralArrow => arrow
-        case _                    => return
-
-      val player = arrow.getShooter match
-        case p: Player => p
-        case _         => return
-
-      val hitBlock = event.getHitBlock
-      if hitBlock == null then return
-
-      val warpPos = hitBlock.getRelative(event.getHitBlockFace)
-
-      if warpPos.getRelative(BlockFace.DOWN).getType.isSolid &&
-        isSafeBlock(warpPos) && isSafeBlock(warpPos.getRelative(BlockFace.UP))
-      then
-        arrow.remove()
-        player.teleport(
-          warpPos.getLocation
-            .add(0.5, 0, 0.5)
-            .setDirection(player.getLocation.getDirection)
-        )
